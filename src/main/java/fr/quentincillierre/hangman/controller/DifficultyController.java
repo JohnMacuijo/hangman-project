77a5793a1cb2ee695c63d31bbc7c5dfb7da76daf
jerekszxc <jerexmedil@gmail.com>
2026-07-23 13:30:28 +0800

    new start screen ui

--- src/main/java/fr/quentincillierre/hangman/controller/DifficultyController.java
+++ src/main/java/fr/quentincillierre/hangman/controller/DifficultyController.java
@@ -1,78 +1,53 @@
 package fr.quentincillierre.hangman.controller;
 
+import java.io.IOException;
+
 import fr.quentincillierre.hangman.model.Difficulty;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class DifficultyController {
+
+    @FXML
+    private MenuButton difficultyMenu;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private Button challengerButton;
 
     private Difficulty selectedDifficulty = Difficulty.EASY;
 
     @FXML
-    private Button easyButton;
-
-    @FXML
-    private Button mediumButton;
-
-    @FXML
-    private Button hardButton;
-
-    @FXML
     public void initialize() {
-        highlightSelectedButton();
+        difficultyMenu.setText("EASY");
     }
-
 
     @FXML
     private void selectEasy() {
         selectedDifficulty = Difficulty.EASY;
-        highlightSelectedButton();
+        difficultyMenu.setText("EASY");
     }
-
 
     @FXML
     private void selectMedium() {
         selectedDifficulty = Difficulty.MEDIUM;
-        highlightSelectedButton();
+        difficultyMenu.setText("MEDIUM");
     }
-
 
     @FXML
     private void selectHard() {
         selectedDifficulty = Difficulty.HARD;
-        highlightSelectedButton();
+        difficultyMenu.setText("HARD");
     }
-
-
-    private void highlightSelectedButton() {
-
-        easyButton.getStyleClass().remove("selected-difficulty");
-        mediumButton.getStyleClass().remove("selected-difficulty");
-        hardButton.getStyleClass().remove("selected-difficulty");
-
-
-        switch (selectedDifficulty) {
-
-            case EASY:
-                easyButton.getStyleClass().add("selected-difficulty");
-                break;
-
-            case MEDIUM:
-                mediumButton.getStyleClass().add("selected-difficulty");
-                break;
-
-            case HARD:
-                hardButton.getStyleClass().add("selected-difficulty");
-                break;
-        }
-    }
-
 
     @FXML
     private void startGame() {
@@ -80,27 +55,72 @@ public class DifficultyController {
         try {
 
             FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/fr/quentincillierre/hangman/application/game-view.fxml")
-            );
+                    getClass().getResource(
+                            "/fr/quentincillierre/hangman/application/game-view.fxml"));
 
             Parent root = loader.load();
 
             GameController controller = loader.getController();
-
             controller.setDifficulty(selectedDifficulty);
 
-
-            Stage stage = (Stage) easyButton.getScene().getWindow();
+            Stage stage = (Stage) startButton.getScene().getWindow();
 
             stage.setScene(new Scene(root, 950, 850));
             stage.show();
-
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    @FXML
+    private void challengerMode() {
+
+        try {
+
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource(
+                            "/fr/quentincillierre/hangman/application/boss-battle-view.fxml"));
+
+            Parent root = loader.load();
+
+            Stage stage = (Stage) challengerButton.getScene().getWindow();
+
+            stage.setScene(new Scene(root, 950, 850));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void about() {
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("About");
+        alert.setHeaderText("Hangman Game");
+
+        alert.setContentText("""
+                Version 1.0
+
+                Developed using JavaFX.
+
+                Features:
+                • Multiple Difficulties
+                • Timer
+                • Sound Effects
+                • Challenger Mode
+                """);
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void exit() {
+        Platform.exit();
+    }
 
     public Difficulty getSelectedDifficulty() {
         return selectedDifficulty;
